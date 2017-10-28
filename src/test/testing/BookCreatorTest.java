b5b781aa7d2e3882ec505a1ca0f001bc20458e2e
yamorist <leiaorgana228@gmail.com>
2017-10-28 23:51:17 +0300

    Minor fixes

--- src/test/testing/BookCreatorTest.java
+++ src/test/testing/BookCreatorTest.java
@@ -1,14 +1,17 @@
 package testing;
 
-import org.junit.Assert;
 import org.junit.Test;
+import serialization.AuthorInfo;
 import serialization.BookCreator;
+
+import static junit.framework.TestCase.assertNotNull;
 
 public class BookCreatorTest {
     @Test
     public void initializeBook() {
         BookCreator bc = new BookCreator();
-        Assert.assertNotNull(bc.initializeBook("Test", "Genre", "Anon", 1111));
+        AuthorInfo authorTestInfo = new AuthorInfo("Test Full Name", "Test City", 1111);
+        assertNotNull(bc.initializeBook("Test", "Genre", "Anon", 1111, authorTestInfo, 1, "map string value"));
     }
 
 }
